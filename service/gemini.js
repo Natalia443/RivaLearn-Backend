2faeed11c46947cda0874a485e13ea1426bb6df7
Natalia443 <96155586+Natalia443@users.noreply.github.com>
2024-06-11 22:11:22 -0300

    Update gemini.js

--- service/gemini.js
+++ service/gemini.js
@@ -27,8 +27,8 @@ class Gemini {
 
   async createStory(words) {
     try {
-      const wordsList = words.join(", ");
-      const prompt = `Escribime una historia que contenga estas palabras y este escrita en el idioma de las mismas: ${wordsList}`;
+      console.log(words);
+      const prompt = `Escribime una historia que contenga estas palabras y este escrita en el idioma de las mismas: ${words}`;
       const result = await this.model.generateContent(prompt);
       return result.response.text();
     } catch (error) {
